70999d4d4928c5edf7fd74692b08f6413a071e58
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-03 14:54:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 1000
+  name_hash.count do |key, value|
+    if name_hash[key] < min_value
+      min_value = name_hash[key]
+    end
+  end
+  name_hash.invert[min_value]
 end
